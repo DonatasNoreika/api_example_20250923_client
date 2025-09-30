fbfbc8db4297d257e5df62f3ecdf5e9f100caba4
DonatasNoreika <donoras@gmail.com>
2025-09-30 18:52:16 +0300

    Sukurtas posts puslapis

--- mysite/client/views.py
+++ mysite/client/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
+def posts(request):
+    r = requests.get("http://127.0.0.1:8000/posts")
+    return render(request, template_name="posts.html", context={"posts": r.json()})
+# print(r.json())
