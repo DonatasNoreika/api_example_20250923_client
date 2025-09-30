a452a34bbda35984fa13bedf3d7a0f0d9ba53a65
DonatasNoreika <donoras@gmail.com>
2025-09-30 18:25:56 +0300

    Initial commit

---
+++ bandymas.py
@@ -0,0 +1,50 @@
+import requests
+token = '1f5b6f643abceb098f278313dba35a37c8c780c5'
+
+# r = requests.get("http://127.0.0.1:8000/posts")
+# print(r.json())
+
+# r = requests.get("http://127.0.0.1:8000/posts/1")
+# print(r.json())
+
+# r = requests.get("http://127.0.0.1:8000/posts/1/comments/")
+# print(r.json())
+
+
+# payload = {
+#     "username": "demo1",
+#     "password": "demo1demo1"
+# }
+#
+# r = requests.post("http://127.0.0.1:8000/api-token-auth/", data=payload)
+# print(r.json())
+
+# payload = {
+#     "title": "Iš Python",
+#     "body": "Iš python tekstas"
+# }
+#
+# headers = {"Authorization": f'Token {token}'}
+#
+# r = requests.post("http://127.0.0.1:8000/posts/", data=payload, headers=headers)
+#
+# print(r.json())
+
+# payload = {
+#     "title": "Iš Python (redaguotas)",
+#     "body": "Iš python tekstas (redaguotas)"
+# }
+#
+# headers = {"Authorization": f'Token {token}'}
+#
+# r = requests.put("http://127.0.0.1:8000/posts/19", data=payload, headers=headers)
+#
+# print(r.json())
+
+
+# headers = {"Authorization": f'Token {token}'}
+#
+# r = requests.delete("http://127.0.0.1:8000/posts/8", headers=headers)
+#
+# print(r.json())
+
