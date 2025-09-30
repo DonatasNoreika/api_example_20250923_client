77ba233abb68fd38fe7c87e572b84edcf98bded4
DonatasNoreika <donoras@gmail.com>
2025-09-30 18:58:27 +0300

    Sukurtas post puslapis

--- mysite/client/views.py
+++ mysite/client/views.py
@@ -6,4 +6,7 @@ import requests
 def posts(request):
     r = requests.get("http://127.0.0.1:8000/posts")
     return render(request, template_name="posts.html", context={"posts": r.json()})
-# print(r.json())
+
+def post(request, post_id):
+    r = requests.get(f"http://127.0.0.1:8000/posts/{post_id}")
+    return render(request, template_name="post.html", context={"post": r.json()})
